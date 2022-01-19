0c380ccb8206f243b8a36a9645f6b3ccd939e6ba
Sinoxolo Sooi <snoxy.sooi@gmail.com>
2022-01-19 21:37:23 +0200

    first commit

---
+++ houses.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        picture: '/img/house1.jpg',
+        type: 'Entire House',
+        town: 'New York',
+        title: 'Beautiful flat in New York'
+    },
+    {
+        picture: '/img/house2.jpg',
+        type: 'Entire House',
+        town: 'Amsterdam',
+        title: ' A flat in Amsterdam with a great view'  
+    }
+]
