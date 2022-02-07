4570709f0dd7cdf4e32e0ba5b073bc28abfa3c51
Sinoxolo Sooi <snoxy.sooi@gmail.com>
2022-02-07 21:57:09 +0200

    new date-picker commit

---
+++ components/Layout.js
@@ -0,0 +1,22 @@
+import Header from "./Header";
+
+export default function Layout(props) {
+  return (
+    <div>
+      <Header />
+      <main> {props.content} </main>
+      <style jsx>
+        {`
+          main {
+            postion: relative;
+            max-width: 56em;
+            background-color: white;
+            padding: 2em;
+            margin: 0 auto;
+            box-sizing: border-box;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
